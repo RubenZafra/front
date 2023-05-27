9548f094e9403d68076038749299f989d951ccd6
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-05-27 15:31:57 +0200

    Gif Container and Gif cards

--- front-tech-challenge/src/components/common/Search/Search.jsx
+++ front-tech-challenge/src/components/common/Search/Search.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react"
 import styles from "../Search/search.module.scss"
 
-
-
 export const Search = () => {
 
     const [isActive, setIsActive] = useState(true)
@@ -13,7 +11,7 @@ export const Search = () => {
         })
       })
   return (
-    <div className={`bg-gray-100 flex flex-col justify-center ${isActive ? ' pt-28 bg-[#557287] ' : 'pt-20 bg-[#9bb0be]'} transition-all duration-500 ease-in-out w-full fixed`}>
+    <div className={`bg-gray-100 flex flex-col justify-center ${isActive ? ' pt-28 bg-[#557287] ' : 'pt-20 bg-[#9bb0be]'} transition-all duration-500 ease-in-out w-full fixed z-10`}>
         <div className="relative p-4 w-full sm:max-w-2xl sm:mx-auto">
             <div className="overflow-hidden z-0 rounded-full relative p-3">
             <form role="form" className="relative flex z-50 bg-white rounded-full">
--- front-tech-challenge/src/components/containers/NavBar/NavBar.jsx
+++ front-tech-challenge/src/components/containers/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ export const NavBar = () => {
 
   return (
     <>
-        <div className={`flex justify-between items-center ${isActive ? 'bg-[#27374D] py-6' : 'bg-[#526D82] py-2'} fixed z-10 w-full px-4 transition-all duration-500 ease-in-out h-26`}>
+        <div className={`flex justify-between items-center ${isActive ? 'bg-[#27374D] py-6' : 'bg-[#526D82] py-2'} fixed z-20 w-full px-4 transition-all duration-500 ease-in-out h-26`}>
           <GiFangsCircle className='text-5xl ml-4 text-white'/>
           <h1 className='text-white font-bold text-2xl'>GIF-MASTER 🍥</h1>
           <div className='flex justify-between p-4'>
